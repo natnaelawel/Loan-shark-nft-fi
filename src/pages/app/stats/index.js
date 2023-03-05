452c2b01ef88624f4cd947db53ddf3df66a29fce
natnaelawel <natnael.awel@gmail.com>
2023-03-05 15:12:07 +0300

    add dapp page

---
+++ src/pages/app/stats/index.js
@@ -0,0 +1,3 @@
+export { default as StatsWETHPage } from './weth'
+export { default as StatsDAIPage } from './usdc'
+export { default as StatsUSDCPage } from './dai'
---
+++ src/pages/app/stats/weth.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+// import LendComponent from '../../../components/App/lend/Lend'
+
+const StatsPage = () => {
+  return <div className='min-h-screen w-screen'>{/* <LendComponent /> */}</div>
+}
+
+export default StatsPage
